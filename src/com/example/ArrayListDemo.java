792f0e1b81b44f9fc7ba5ae485a7ba3bb0dad2d2
SandeshPatil8 <patilsandeshbalu@gmail.com>
2022-12-24 18:02:51 +0530

    .gitignore

---
+++ src/com/example/ArrayListDemo.java
@@ -0,0 +1,15 @@
+package com.example;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArrayListDemo {
+
+	public static void main(String[] args) {
+
+		List list = new ArrayList();
+		list.add("sandesh");
+		list.add("deepak");
+	}
+
+}
