f57be856b6cca6b63271e4a93e02b0917104a86c
SandeshPatil8 <patilsandeshbalu@gmail.com>
2022-12-26 12:53:01 +0530

    local change

--- src/com/example/LinkedListDemo.java
+++ src/com/example/LinkedListDemo.java
@@ -8,10 +8,8 @@ public class LinkedListDemo {
 	public static void main(String[] args) {
 		
 		System.out.println("hi..i have made some changes into clone copy from remote repository");
-
 		System.out.println("hi..change from remote repository");
-
-		System.out.println("this is change from master branch");
+		System.out.println("local change");
 		
 		
 	}
